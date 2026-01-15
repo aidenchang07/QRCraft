a4297488fa69835bdc3ba6916bad744f0fa3086c
AidenChang <asdf62210@gmail.com>
2026-01-15 20:53:17 +0800

    使用 data object
    顯示log可看到內容而不會是記憶體位置

--- app/src/main/java/com/aiden/qrcraft/presentation/ScanState.kt
+++ app/src/main/java/com/aiden/qrcraft/presentation/ScanState.kt
@@ -4,5 +4,5 @@ package com.aiden.qrcraft.presentation
  * Created by AidenChang on 2025/12/5
  */
 sealed class ScanState {
-    object Loading : ScanState()
+    data object Loading : ScanState()
 }
